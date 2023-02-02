e9434da7cfa706b29775e28de01bf6aa32db0593
Philip Pretorius <philippretorius.79@gmail.com>
2023-02-02 15:59:59 +0200

    Traversy Media React Tutorial - added react-task-tracker app

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -6,3 +6,25 @@ const myFirstElement = <h1>Hello React!</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(myFirstElement);
+
+// Component as a function
+
+export const header = () => {
+    return (
+        <div>
+            <h1>Awe ma se kinders!</h1>
+        </div>
+    )
+}
+
+// Component as a class
+
+export default class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Hoe lyk it?</h2>
+            </div>
+        )
+    }
+}
