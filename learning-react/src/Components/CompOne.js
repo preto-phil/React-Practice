725ba65117de7badc8e253f3c5856f82a46ada88
Philip Pretorius <philippretorius.79@gmail.com>
2023-02-06 15:36:48 +0200

    Moeg en deruemekaar

---
+++ learning-react/src/Components/CompOne.js
@@ -0,0 +1,7 @@
+
+
+function CompOne(props) {
+    return <h2>Hello {this.props.name}</h2>
+}
+
+export default CompOne
---
+++ learning-react/src/Components/Heading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Heading = () => {
+  return (
+    <h1>Hello, World!</h1>
+  )
+}
+
+export default Heading
