bd15570dad41e021299e5202e890201eb6307423
Philip Pretorius <philippretorius.79@gmail.com>
2023-09-25 22:43:45 +0200

    Intro states lesson fin

--- TOP-React/States-Effects/Intro-States/src/App.jsx
+++ TOP-React/States-Effects/Intro-States/src/App.jsx
@@ -1,13 +1,16 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 
 const COLORS = ["pink", "green", "blue", "yellow", "purple"];
 
 function App() {
   const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [count, setCount] = useState(0);
+
 
   const onButtonClick = (color) => () => {
     setBackgroundColor(color);
+    setCount(count + 1);
   };
 
   return (
@@ -17,6 +20,7 @@ function App() {
         backgroundColor,
       }}
     >
+      <div>Count: {count}</div>
       {COLORS.map((color) => (
         <button
           type="button"
