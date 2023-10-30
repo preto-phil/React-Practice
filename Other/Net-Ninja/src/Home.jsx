7ccef7ebed587f49461b49067ed485b1292c0f1a
preto-phil <104874252+preto-phil@users.noreply.github.com>
2023-10-30 18:29:08 +0200

    React Net Ninja Tut #7 Fin

--- Other/Net-Ninja/src/Home.jsx
+++ Other/Net-Ninja/src/Home.jsx
@@ -1,7 +1,19 @@
 const Home = () => {
+  const handleClick = (e) => {
+    console.log('Hello Ninjas');
+    alert('Hello Ninjas', e);
+  }
+
+  const handleClickAgain = (name) => {
+    console.log('Hello ' + name);
+    alert('Hello ' + name);
+  }
+
   return (
     <div className="home">
       <h2>Homepage</h2>
+      <button onClick={handleClick}>Click me</button>
+      <button onClick={() => handleClickAgain('Mario')}>Click me again</button>
     </div>
   );
 }
