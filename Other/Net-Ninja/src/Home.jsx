9983ab844b8a17e9cbeadd68530aac79c1cdf7b3
preto-phil <104874252+preto-phil@users.noreply.github.com>
2023-10-30 18:29:22 +0200

    React Net Ninja Tut #10 Fin

--- Other/Net-Ninja/src/Home.jsx
+++ Other/Net-Ninja/src/Home.jsx
@@ -1,19 +1,20 @@
 import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState('mario');
-  const [age, setAge] = useState(25);
-
-  const handleClick = () => {
-    setName('luigi');
-    setAge(30);
-  }
+  const [blogs, setBlogs] = useState([
+    { title: 'A', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'B', body: 'lorem ipsum...', author: 'kong', id: 2 },
+    { title: 'C', body: 'lorem ipsum...', author: 'luigi', id: 3 }
+  ]);
 
   return (
     <div className="home">
-      <h2>Homepage</h2>
-      <p>{ name } is { age } years old</p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => {
+        <div className="blog-preview" key={blog.id}>
+          <h2>{ blog.title }</h2>
+          <p>Written by { blog.author }</p>
+        </div>
+      })}
     </div>
   );
 }
