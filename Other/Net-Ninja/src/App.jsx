a1cb97886a215c5d63ec64a10c6c96bf84417e7c
preto-phil <104874252+preto-phil@users.noreply.github.com>
2023-10-30 18:28:53 +0200

    Net Ninja React Tutorial Lesson 4 completed

--- Other/Net-Ninja/src/App.jsx
+++ Other/Net-Ninja/src/App.jsx
@@ -1,33 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const title = 'Welcome to the new blog';
+  const likes = 50;
+  const link = 'http://www.google.com';
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="App">
+      <h1>{ title }</h1>
+      <p>Liked { likes } times</p>
+      <p>{ 10 }</p>
+      <p>{ "Hello Ninjas" }</p>
+      <p>{ Math.random() * 10 }</p>
+
+      <a href={link} target="_blank" rel='noreferrer'>Google Site</a>
+    </div>
     </>
   )
 }
